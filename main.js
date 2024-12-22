40b23282c3cc2a870e2c261e139c5e2ca44b175c
irvan <irvanprima2102@gmail.com>
2024-12-22 16:29:20 +0700

    add delete mechanism, add css to list

--- main.js
+++ main.js
@@ -3,20 +3,27 @@ const list = document.getElementById("list")
 function onFormSubmit(e) {
     e.preventDefault()
     const inputValue = e.target.children[0].value
-    console.log(inputValue)
-
+    // console.log(inputValue)
     list.appendChild(createListItem(inputValue))
+
+}
+
+function deleteListItem (e){
+    const parent = (e.target.parentElement)
+    list.removeChild(parent)
 
 }
 
 function createListItem (inputValue){
     const item = document.createElement("div")
     item.innerHTML = `
-        <div>
+        <div class="list-item-detail">
             <input type="checkbox" >
             <span>${inputValue}</span>
         </div>
-        <button>X</button>
+        <button onclick = "deleteListItem(event)">X</button>
     `
+
+    item.setAttribute("class", "list-item")
     return item
 }
